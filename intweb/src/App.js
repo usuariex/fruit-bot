fcc0c5dd3e59e8845b4f036d25cd8df0738f4569
Jimmy Montoya <jimmymontoyam@gmail.com>
2025-11-16 00:57:30 -0500

    Arreglando el React Base para la Interfaz Web

---
+++ intweb/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
