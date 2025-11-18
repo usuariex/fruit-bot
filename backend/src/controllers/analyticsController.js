bfaea1463abaebd55ac4ae3a0a7f7e91ac4bfdf4
Jimmy Montoya <jimmymontoyam@gmail.com>
2025-11-18 01:44:28 -0500

    Creando estructura para IntWeb

---
+++ backend/src/controllers/analyticsController.js
@@ -0,0 +1,12 @@
+import { conexionbd } from "../db.js";
+
+//Funcion para devolver las frutas más vistas
+export const getTopFrutas = async (req, res) => {
+    try {
+        const [rows] = await conexionbd.query(
+            
+        )
+    } catch (error) {
+        
+    }
+}
