15f8be74d2dc223a8943e8702b8658bc403f4b95
dibujito programador <tumama@gmail.com>
2025-11-19 04:38:54 -0500

    scaner

--- android/app/src/main/java/com/devs/frutybot/common/Config.java
+++ android/app/src/main/java/com/devs/frutybot/common/Config.java
@@ -2,9 +2,9 @@ package com.devs.frutybot.common;
 
 public class Config {
     /*mi ip local*/
-    public static final String BASE_URL = "http://192.168.100.229:3020";
+    /*public static final String BASE_URL = "http://192.168.100.229:3020";*/
 
     /*public static final String BASE_URL = "http://192.168.100.176:3020";*/
 
-    /*public static final String BASE_URL = "http://192.168.100.8:3020";*/
+    public static final String BASE_URL = "http://192.168.100.6:3020";
 }
